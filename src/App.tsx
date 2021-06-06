01d7d0718e1a96a02731fe4097f2b2a96da3626a
mohamedabdifitah <learntecnology30@email.com>
2021-06-06 20:12:19 +0300

    v1.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import  React from 'react';
 import './App.css';
-
+import {BrowserRouter as Router , Switch,Route,Link} from "react-router-dom"
+import Home from "./component/Home"
+import New from "./component/New"
+import Header from "./component/Header/Header"
+import Sidebar from "./component/Sidebar/Sidebar"
+import Windows from "./component/Window/Windows"
+import Terminal from "./component/Terminal/Terminal"
+import Navbar from "./component/Sidebar/Navbar/Navbar"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar />
+       <Header />
+       <Sidebar />
+       <Windows />
+       <Terminal />
     </div>
   );
 }
