d80b072c92af906f8e67be75576f38089b97375a
mohamedabdifitah <learntecnology30@email.com>
2021-07-01 19:46:12 +0300

    adding Editor

--- src/component/Window/Editor/Editor.tsx
+++ src/component/Window/Editor/Editor.tsx
@@ -1,12 +1,53 @@
 import "./Editor.css"
-import EditorViewer from "./EditorViewer"
-import useLocalStorage from '../hooks/useLocalStorage'
+import {UnControlled as CodeMirror} from 'react-codemirror2'
+import React,{useRef,useEffect,useState} from "react";
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import EditorComp  from "./EditorComp";
+import useLocalStorage from "./useLocalStorage"
+function Editor() {
+ var code = "HELLO WORLD "
+ function TextInputHandler(e:React.FormEvent<HTMLOListElement>) {
+  const lines = [e.target]
+  lines.map((index,data) => {
+   console.log(data,index)
+   
+  })
+ }
+ /*const ListRef = useRef<HTMLOLlistElement>(null)*/
+ const [openFile,setOpenFile] = useState("<ol></ol>")
+ /*
+ const Test = () => {
+  setOpenFile(true)
+  if (openFile) {
+   console.log(ListRef?.current?.firstElementChild())
 
-const Editor = () => {
- const [html, setHtml] = useLocalStorage('html', '')
- const [css, setCss] = useLocalStorage('css', '')
- const [js, setJs] = useLocalStorage('js', '')
- const [srcDoc, setSrcDoc] = useState('')
+  }
+ }
+ */
+ /*
+  */
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
 
  return (
   <div className="Editor__section">
@@ -20,14 +61,14 @@ const Editor = () => {
       import </h4>
       </td>
       <td contentEditable="true">
-      <h4>
+      <h4 className="primary__word">
        data
        </h4>
       </td>
      <td contentEditable="true"><h4 className="keyword__word" 
      >from </h4> </td>
      <td contentEditable="true">
-     <h4>
+     <h4 className="primary__word">
      "./data"
      </h4>
      </td>
@@ -36,14 +77,50 @@ const Editor = () => {
     </ol>
    </table>
 
-   <EditorViewer />
+   <CodeMirror
+    value={code}
+    options={{
+     mode: 'xml',
+     theme: 'darcula',
+     lineNumbers: true
+    }}
+    
+   />
    */}
-   <EditorViewer
-    language="css"
-    displayName="CSS"
-    value={css}
-    onChange={setCss}
-    />
+   <div 
+    className="code__section"
+   >
+    <EditorComp
+          language="javascript"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+     />
+    {/*
+    <CodeMirror
+     value={openFile}
+     options={{
+      mode: 'xml',
+     theme: 'material',
+     lineNumbers: true
+    }}
+    onBeforeChange={handleChange}
+   />
+   "/}
+   {/*
+    <ol 
+    className="line__numbers"
+    contentEditable="true"
+    onInput={(e) => {                                              TextInputHandler(e)                                          }}
+    
+    
+    >
+    <li className="line__numbers">
+     {createCode()}
+    </li>
+   </ol>
+   */}
+   </div>
   </div>
 
  )
---
+++ src/component/Window/Editor/EditorComp.tsx
@@ -0,0 +1,64 @@
+import React, { useState,useRef } from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+
+interface Props {
+language:string;
+displayName:string;
+value:any;
+onChange:any
+}
+
+export default function EditorComp(props:Props) {
+  const {
+    language,
+    displayName,
+    value,
+    onChange
+  } = props
+  const [open, setOpen] = useState(true)
+
+  function handleChange(editor:any, data:any, value:any) {
+    onChange(value)
+  }
+  /*
+  const codemirrorRef = React.useRef();
+  React.useEffect(() => {
+  const current = codemirrorRef.current.editor.display.wrapper.style.height = "1000px";
+});
+ */
+  /*
+  const EditorRef = useRef()
+  */
+
+  return (
+  <div className="editor__comp">
+   <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+
+        className="code-mirror-wrapper"
+        options={{
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true,
+	  matchBrackets: true,
+	  smartIndent: true,
+	  autoCloseBrackets: true,
+	  autoCursor:true,
+	  autocomplete:true,
+	  extraKeys: {
+          "Space": "autocomplete"
+        }
+        }}
+	
+      />
+   </div>
+  )
+
+  }
